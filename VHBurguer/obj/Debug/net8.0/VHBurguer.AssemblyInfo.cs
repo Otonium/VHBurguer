b0fe50c1ca7c9626765d053bf85efc94fdc883e7
ANTONIO CARLOS VIEIRA SANTOS <47786092803@info.local>
2026-02-18 10:44:31 -0300

    :construction:

--- VHBurguer/obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
+++ VHBurguer/obj/Debug/net8.0/VHBurguer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c051e75aeb0fe94546c8fa71b9fe2bb5a4e818bd")]
 [assembly: System.Reflection.AssemblyProductAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("VHBurguer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
